45791af4186d202ec0da14ec83c7490b66815ea6
It_is_Sean <sean@Seans-MacBook-Pro.local>
2025-11-16 02:46:33 +0800

    fix: now the speed would not decrease after the user reach his target

--- util/speed.py
+++ util/speed.py
@@ -1,6 +1,8 @@
 import config
 import math
 def current_speed(current_loop):
+    if current_loop >= config.config.target_loops:
+        return config.config.min_speed
     lam = (1 / config.config.target_loops) * math.log(config.config.max_speed/ config.config.min_speed)
     return config.config.max_speed* math.exp(-lam * current_loop)
 
